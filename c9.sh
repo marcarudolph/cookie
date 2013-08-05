dfecef97ac7d91b00613edb89cdf9ad78ad88a0b
Marc Rudolph <marc.a.rudolph@gmail.com>
2013-08-05 17:36:38 +0200

    c9 fix for lost port

--- c9.sh
+++ c9.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
+C9_PORT=$COOKIE_C9_PORT
 SCRIPT=$(readlink -f $0)
 BASEDIR=$(dirname $SCRIPT)
 ABSBASEDIR=$(readlink -f $BASEDIR)
 
-nohup /var/www/cloud9/bin/cloud9.sh -l 0.0.0.0 -p $COOKIE_C9_PORT -w $ABSBASEDIR > /dev/null 2>&1 &
+nohup /var/www/cloud9/bin/cloud9.sh -l 0.0.0.0 -p $C9_PORT -w $ABSBASEDIR > /tmp/c9.log 2>&1 &
