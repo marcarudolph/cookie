d95717dfc5cc89adf0c5d18bb3a3b42e4befab93
Marc Rudolph <marc.a.rudolph@gmail.com>
2013-08-08 18:58:56 +0200

    Some more changes to c9 scripts

--- c9.sh
+++ c9.sh
@@ -5,4 +5,4 @@ SCRIPT=$(readlink -f $0)
 BASEDIR=$(dirname $SCRIPT)
 ABSBASEDIR=$(readlink -f $BASEDIR)
 
-nohup /var/www/cloud9/bin/cloud9.sh -l 0.0.0.0 -p $C9_PORT -w $ABSBASEDIR > /tmp/c9.log 2>&1 &
+nohup /var/www/cloud9/bin/cloud9.sh -l 0.0.0.0 -p 1506 -w $ABSBASEDIR > /tmp/c9.log 2>&1 &
--- c9session.sh
+++ c9session.sh
@@ -3,5 +3,6 @@
 SCRIPT=$(readlink -f $0)
 BASEDIR=$(dirname $SCRIPT)
 ABSBASEDIR=$(readlink -f $BASEDIR)
+$C9_PORT=$COOKIE_C9_PORT 
 
-/var/www/cloud9/bin/cloud9.sh -l 0.0.0.0 -p $COOKIE_C9_PORT -w $ABSBASEDIR
+/var/www/cloud9/bin/cloud9.sh -l 0.0.0.0 -p 1506 -w $ABSBASEDIR
