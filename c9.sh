f84f2ca768d865bdd10351610fbbe1d336c0d44a
Marc Rudolph <marc.a.rudolph@gmail.com>
2013-08-26 18:39:25 +0200

    cloud9 authentication

--- c9.sh
+++ c9.sh
@@ -5,4 +5,4 @@ SCRIPT=$(readlink -f $0)
 BASEDIR=$(dirname $SCRIPT)
 ABSBASEDIR=$(readlink -f $BASEDIR)
 
-forever start /var/www/cloud9/server.js -w $ABSBASEDIR -l 0.0.0.0 -p $COOKIE_C9_PORT -a x-www-browser
+forever start /var/www/cloud9/server.js -w $ABSBASEDIR -l 0.0.0.0 -p $COOKIE_C9_PORT -a x-www-browser --username $USER --password $COOKIE_C9_PWD
