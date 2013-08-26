1fe7ecef1a3f7bb534ab49b5e8f1a7ec755080c7
Marc Rudolph <marc.a.rudolph@gmail.com>
2013-08-26 17:56:14 +0200

    cloud9 start via forever

--- c9session.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-SCRIPT=$(readlink -f $0)
-BASEDIR=$(dirname $SCRIPT)
-ABSBASEDIR=$(readlink -f $BASEDIR)
-$C9_PORT=$COOKIE_C9_PORT 
-
-/var/www/cloud9/bin/cloud9.sh -l 0.0.0.0 -p 1506 -w $ABSBASEDIR
