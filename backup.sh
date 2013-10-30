d1905bffd2bbe97b7247a78a39bcb954b9543533
Marc Rudolph <marc.a.rudolph@gmail.com>
2013-10-30 17:11:57 +0100

    Backups to dropbox

---
+++ backup.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+TMP_DIR="/tmp"
+DATE=$(date +"%Y%m%d_%H%M")
+BKP_FILE="$TMP_DIR/cookiedb_$DATE.tar"
+DUMP_DIR="$TMP_DIR/dump"
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+DROPBOX_UPLOADER="$SCRIPTPATH/dropbox_uploader.sh"
+
+cd "$TMP_DIR"
+mongodump
+
+
+tar cf "$BKP_FILE" $DUMP_DIR
+gzip "$BKP_FILE"
+
+$DROPBOX_UPLOADER -f ~/.dropbox_uploader upload "$BKP_FILE.gz"
+
+rm -fr "$BKP_FILE.gz"
+rm -r "$DUMP_DIR"
+
