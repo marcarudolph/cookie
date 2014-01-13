eb5dff2141fff7953a4a0779cd45e83e0f00b87a
Marc Rudolph <marc.a.rudolph@gmail.com>
2014-01-13 19:04:24 +0100

    Update local dev config

--- config/marc.js
+++ config/marc.js
@@ -1,9 +1,9 @@
 
 var config = {
     server: {
-        port:   1507,
+        port:   1506,
         ip: "0.0.0.0",
-        baseurl: 'http://maiden.eztwo.com:1507' 
+        baseurl: 'http://localhost:1506' 
     },
     databases: {
         recipes: { host: "127.0.0.1", port: 27017, db: "mgmt", collectionName: "recipes"},
