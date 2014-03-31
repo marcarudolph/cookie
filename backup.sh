2297a8183f4813699927c65dbecf12aeeb327c62
marcarudolph <marc.a.rudolph@gmail.com>
2014-03-31 20:12:42 +0200

    Add backup step for pictures

--- backup.sh
+++ backup.sh
@@ -8,9 +8,10 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 DROPBOX_UPLOADER="$SCRIPTPATH/dropbox_uploader.sh"
 
+
+#Backup Database
 cd "$TMP_DIR"
 mongodump
-
 
 tar cf "$BKP_FILE" $DUMP_DIR
 gzip "$BKP_FILE"
@@ -19,4 +20,7 @@ $DROPBOX_UPLOADER -f ~/.dropbox_uploader upload "$BKP_FILE.gz"
 
 rm -fr "$BKP_FILE.gz"
 rm -r "$DUMP_DIR"
+
+#Backup Images
+$DROPBOX_UPLOADER -s -f ~/.dropbox_uploader upload "/var/www/cookie/pictures/" "/picturebackup"
 
