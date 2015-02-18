08386763f439b336dd400cb1a99ec7452b79cdc7
Marc Rudolph <mr@ingentis.de>
2015-02-18 17:15:34 +0100

    ENV variable for base URL

--- config/cookie-config.js
+++ config/cookie-config.js
@@ -3,6 +3,7 @@ var dbCreds = process.env.ES_CREDS ? process.env.ES_CREDS + "@" : "",
     dbHost = process.env.ES_HOST || "es:9242",
     uploadTempPath = process.env.UPLOAD_TEMP_PATH || "/tmp",
     picturesPath = process.env.PICTURES_PATH || "./pics",
+    baseurl = process.env.BASE_URL || "http://localhost:8088",
     connString = dbCreds + dbHost;
 
 
@@ -10,7 +11,7 @@ var config = {
     server: {
         port:   8088,
         ip: "0.0.0.0",
-        baseurl: 'http://localhost:8088',
+        baseurl: baseurl,
         uploadTempPath: uploadTempPath,
         cacheMaxAge: 1209600
     },
